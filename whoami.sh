50e1e8144bb46e41031a1329ae8581a072d54a7d
Reynaldo Paulo Rivera <reynaldopaulorivera@Reynaldos-MacBook-Air.local>
2024-11-22 06:04:32 -0800

    Add comment to whoami.sh script

--- whoami.sh
+++ whoami.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 # This script will check if the current user is root or not.
 
+# Fetch current user
 current_user=$(whoami)
 
+# Check user if root or not
 if [ "$current_user" == "root" ]; then
     echo "You are logged in as root.
 else
