6dc04c0b3d6c318e5638459d220772b697de9ea3
Reynaldo Paulo Rivera <reynaldopaulorivera@Reynaldos-MacBook-Air.local>
2024-11-22 06:03:13 -0800

    Update whoami.sh script admin to root

--- whoami.sh
+++ whoami.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-# This script will check if the current user is admin or not.
+# This script will check if the current user is root or not.
 
 current_user=$(whoami)
 
-if [ "$current_user" == "admin" ]; then
-    echo "You are logged in as admin."
+if [ "$current_user" == "root" ]; then
+    echo "You are logged in as root.
 else
     echo "You are logged in as $current_user."
 fi
